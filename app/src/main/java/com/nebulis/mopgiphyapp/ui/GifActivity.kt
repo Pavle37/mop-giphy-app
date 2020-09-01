1d3e07aa8ac28e4baa6a07c5b920318ee3f1c653
Pavle Pavlov <pavle.pavlov@infrakit.com>
2020-09-02 01:37:19 +0200

    Changed launcher image, added navigation controller, app bar and edited underlying theme

---
+++ app/src/main/java/com/nebulis/mopgiphyapp/ui/GifActivity.kt
@@ -0,0 +1,24 @@
+package com.nebulis.mopgiphyapp.ui
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.nebulis.mopgiphyapp.R
+
+
+class GifActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gif)
+
+        setSupportActionBar(findViewById(R.id.topAppBar))
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.top_app_bar, menu)
+        return true
+    }
+}
---
+++ app/src/main/java/com/nebulis/mopgiphyapp/ui/GridViewModel.kt
@@ -0,0 +1,7 @@
+package com.nebulis.mopgiphyapp.ui
+
+import androidx.lifecycle.ViewModel
+
+class GridViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
